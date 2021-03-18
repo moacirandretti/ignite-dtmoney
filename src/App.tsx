a86475de60e3073c3d4af474af1086900b6f6758
moacirandretti <moacir.andretti@gmail.com>
2021-03-17 21:38:41 -0300

    ADD: Configuration of component Modal. Model Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { GlobalStyle } from './assets/styles/global'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
+import Modal from 'react-modal'
+import React, { useState } from 'react'
+import { NewTransactionModal } from './components/NewTransactionModal'
+
+Modal.setAppElement('#root')
 
 export const App: React.FC = () => {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+		false
+	)
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true)
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false)
+	}
 	return (
 		<>
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
+
+			<NewTransactionModal
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			/>
 			<GlobalStyle />
 		</>
 	)
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
+import Modal from 'react-modal'
 import { Container, Content } from './styles'
+interface HeaderProps {
+	onOpenNewTransactionModal: () => void
+}
 
-export const Header: React.FC = () => {
+export const Header: React.FC<HeaderProps> = ({
+	onOpenNewTransactionModal
+}) => {
 	return (
 		<Container>
 			<Content>
 				<img src={logoImg} alt="logo gt money" />
-				<button type="button">Nova Transação</button>
+				<button type="button" onClick={onOpenNewTransactionModal}>
+					Nova Transação
+				</button>
 			</Content>
 		</Container>
 	)
